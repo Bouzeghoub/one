409b4ef35a3a29ba9f2685145aae70b089471dc4
Daniel Clavijo Coca <dclavijo@opennebula.systems>
2018-11-21 14:14:51 -0600

    F #1684: Fixed wrong versions

--- src/cloud/common/CloudClient.rb
+++ src/cloud/common/CloudClient.rb
@@ -50,7 +50,7 @@ end
 module CloudClient
 
     # OpenNebula version
-    VERSION = '5.6.2'
+    VERSION = '5.7.80'
 
     # #########################################################################
     # Default location for the authentication file
